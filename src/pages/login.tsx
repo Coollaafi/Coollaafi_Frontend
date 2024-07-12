9855ff9fc133fe434912c626aade8ce4d5743731
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-07-13 05:13:03 +0900

    feat: #2 kakaoUrl 연결

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,11 @@
 export default function LoginPage() {
-  return <>로그인페이지</>;
+  const Rest_api_key = process.env.Rest_api_key;
+  const redirect_url = 'http://localhost:3000';
+  const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_url}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoUrl;
+  };
+
+  return <button onClick={handleLogin}>카카오로 시작하기</button>;
 }
