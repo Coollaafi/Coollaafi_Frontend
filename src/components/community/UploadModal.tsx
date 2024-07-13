21daaccf0cfe1b12d237cf8e61618ae658733fe2
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-07-14 04:40:00 +0900

    feat: #4 모달 페이지2 UI 완료

--- src/components/community/UploadModal.tsx
+++ src/components/community/UploadModal.tsx
@@ -39,6 +39,16 @@ const Box = styled.div<{ name: string }>`
     props.name == 'title' ? '16px 0 84px 0' : '0 0 106px 0'};
 `;
 
+/**임의로 36.5px로 margin 줌 원래는 42px인데, 안맞아..**/
+const SecondBox = styled.div<{ name: string }>`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-family: 'Noto';
+  margin: ${(props) =>
+    props.name == 'image' ? '-54px 0 16px 0' : '0 0 36.5px 0'};
+`;
+
 const ImageBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -47,7 +57,7 @@ const ImageBox = styled.div`
   align-items: center;
   width: 296px;
   height: 200px;
-  background: #fbfbfb;
+  background-color: #fbfbfb;
   border: 1px solid #f4f4f4;
   font-family: 'Noto';
   font-size: 14px;
@@ -67,6 +77,79 @@ const Icon = styled.img`
   height: 16px;
 `;
 
+const ResultBoxs = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
+
+const ResultBox = styled.div`
+  width: 140px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  text-align: center;
+`;
+
+const Result = styled.div`
+  width: 140px;
+  height: 200px;
+  position: relative;
+  background-color: #fbfbfb;
+  border: none;
+`;
+
+//font 바꾸면 font-size 12px로 수정 필요
+const StoreBtn = styled.button`
+  font-family: 'Noto';
+  font-size: 10px;
+  color: #fff;
+  padding: 10px;
+  background-color: #000000;
+  border: none;
+  border-radius: 40px;
+  position: absolute;
+  bottom: 12px;
+  left: 7px;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 62px;
+  background: #fbfbfb;
+  font-family: 'Noto-Thin';
+  font-size: 12px;
+  padding: 12px 10px;
+  border: none;
+  outline: none;
+  resize: none;
+  work-break: break-all;
+  &::placeholder {
+    color: #c7c7c7;
+  }
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const CheckBoxs = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const CheckBox = styled.button<{ isChecked: boolean }>`
+  padding: 10px;
+  background-color: ${(props) => (props.isChecked ? '#000000' : '#fbfbfb')};
+  border: ${(props) =>
+    props.isChecked ? '1px solid #000000' : '1px solid #f4f4f4'};
+  border-radius: 40px;
+  font-family: 'Noto';
+  font-size: 12px;
+  color: ${(props) => (props.isChecked ? '#ffffff' : '#9f9f9f')};
+`;
+
 const Button = styled.button<{ isDone: boolean }>`
   width: 296px;
   height: 48px;
@@ -80,6 +163,7 @@ type UploadModalProps = {
 
 export default function UploadModal({ closeModal }: UploadModalProps) {
   const [isDone, setIsDone] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
   return (
     <Container>
@@ -99,6 +183,29 @@ export default function UploadModal({ closeModal }: UploadModalProps) {
           </ImageBox>
           <Detail>파일을 재선택하려면 위의 버튼을 눌러주세요</Detail>
         </Box>
+        {/*<SecondBox name="image">
+          <ResultBoxs>
+            <ResultBox>
+              <Detail>OOTD</Detail>
+              <Result></Result>
+            </ResultBox>
+            <ResultBox>
+              <Detail>WOT LOOK BOOK</Detail>
+              <Result>
+                <StoreBtn>왓룩북 이미지 저장하기</StoreBtn>
+              </Result>
+            </ResultBox>
+          </ResultBoxs>
+        </SecondBox>
+        <SecondBox name="content">
+          <Detail>룩북 게시글을 작성해주세요</Detail>
+          <TextArea placeholder="내용을 작성해주세요." />
+          <CheckBoxs>
+            <CheckBox isChecked={isChecked}>더웠어요🥵</CheckBox>
+            <CheckBox isChecked={isChecked}>딱 좋았어요😖</CheckBox>
+            <CheckBox isChecked={isChecked}>추웠어요🥶</CheckBox>
+          </CheckBoxs>
+        </SecondBox>*/}
         <Button isDone={isDone}>룩북으로 변경하기</Button>
       </ModalBox>
     </Container>
