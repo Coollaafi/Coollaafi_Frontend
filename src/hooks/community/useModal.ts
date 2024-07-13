6183c879fa2848b7cf89bff61eafb13ec39ffe05
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-07-14 02:34:27 +0900

    feat: #4 모달창 오픈 시, 스크롤 방지

--- src/hooks/community/useModal.ts
+++ src/hooks/community/useModal.ts
@@ -3,8 +3,14 @@ import { useState } from 'react';
 export default function useModal() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
+  const openModal = () => {
+    setIsOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+    document.body.style.overflow = 'unset';
+  };
 
   return {
     isOpen,
