cd5393ade8ba15d4086293d15fe57c2b56fd9103
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-09-27 15:43:31 +0900

    feat: #10 modal 창에 닉네임 관련 info 데이터 넣음

--- src/components/home/InfoModal.tsx
+++ src/components/home/InfoModal.tsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
-import { CTA_button_med } from 'styles/typography';
+import {
+  CTA_button_med,
+  Desc_120_med,
+  Main_title_med,
+} from 'styles/typography';
+import data from '../../data/level.json';
+import NicknameBox from 'components/NicknameBox';
 
 const Container = styled.div`
   width: 360px;
@@ -29,6 +35,29 @@ const CloseBtn = styled.text`
   cursor: pointer;
 `;
 
+const TitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+  margin: 15px 0 32px 0;
+`;
+
+const DataBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 28px;
+  margin-bottom: 15px;
+`;
+
+const LevelBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+  width: 85px;
+`;
+
 type InfoModalProps = {
   closeModal: () => void;
 };
@@ -41,6 +70,25 @@ export default function InfoModal({ closeModal }: InfoModalProps) {
         <CloseBtn onClick={closeModal}>
           <CTA_button_med>닫기</CTA_button_med>
         </CloseBtn>
+        <TitleBox>
+          <Main_title_med>WOT 패피로 성장하기</Main_title_med>
+          <Desc_120_med>
+            업로드한 왓 룩북 이미지를 통해 패피로 성장해보세요!
+          </Desc_120_med>
+        </TitleBox>
+        {data.level.map((item, idx) => (
+          <DataBox key={idx}>
+            <LevelBox>
+              <CTA_button_med>{`Lv ${item.id}`}</CTA_button_med>
+              <Desc_120_med>
+                {item.max == -1
+                  ? `${item.min}장 -`
+                  : `${item.min} - ${item.max}장`}
+              </Desc_120_med>
+            </LevelBox>
+            <NicknameBox nickname={item.nickname} />
+          </DataBox>
+        ))}
       </ModalBox>
     </Container>
   );
